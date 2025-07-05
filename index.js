663083f401226cba10fc9b306ce8f93decce9722
Varun Subramaniam <131638793+varun-subramaniam@users.noreply.github.com>
2025-07-05 21:30:02 +0530

    mongo changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import {logger} from "./logger.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -16,6 +17,20 @@ app.get('/:value', (req, res) => {
 
 
 app.listen(8080, async () => {
+    logger.info('Starting the server...');
+    await connectDB()
     logger.info('Listening on port ' + process.env.SERVER_PORT);
     logger.info("Server started successfully");
 });
+
+async function connectDB(){
+    logger.info("Connecting to MongoDB...");
+    try {
+        await mongoose.connect("mongodb://localhost:27017/dummy", { useNewUrlParser: true, useUnifiedTopology: true });
+        logger.info("Connected to MongoDB successfully");
+    } catch (error) {
+        logger.error("Error connecting to MongoDB: ", error);
+    }
+    logger.info("Connected to MongoDB...");
+    console.log("Server Started Successfully. See log file for more detailed info")
+}
